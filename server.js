72df1bf567792adec227de558309e284f054524e
StuMartin22 <STUCODES22@GMAIL.COM>
2022-05-18 18:34:56 -0500

    added port to server.js, got some starter code for api routes.

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
-app.listen(PORT, () => console.log(`Listening on PORT: PORT`));
+app.listen(PORT, () => console.log(`Listening on PORT: 3001`));
