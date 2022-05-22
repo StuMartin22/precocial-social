d525175ae2f6e3dbd318b7257ce63e10790c9642
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-05-22 12:11:35 -0500

    added starter code for reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,29 @@
+
+const { Schema, model } = require("mongoose");
+
+const reactionSchema = new Schema(
+  {
+    reactionBody: {
+      type: String,
+      required: true,
+      max_length: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+    versionKey: false
+  }
+);
+
+module.exports = reactionSchema;
