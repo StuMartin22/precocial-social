2f5d48f9fde1eff50805407197b29fecc5a4d72c
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-05-22 17:51:28 -0500

    updated thought routes.

--- routes/api/thought.js
+++ routes/api/thought.js
@@ -1,20 +1,24 @@
 const router = require('express').Router();
 const {
-  getCourses,
-  getSingleCourse,
-  createCourse,
-  updateCourse,
-  deleteCourse,
-} = require('../../controllers/courseController.js');
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  terminateReaction,
+} = require('../../controllers/thoughtController');
 
-// /api/courses
-router.route('/').get(getCourses).post(createCourse);
+// /api/user
+router.route('/').get(getThoughts).post(createThought);
 
-// /api/courses/:courseId
-router
-  .route('/:courseId')
-  .get(getSingleCourse)
-  .put(updateCourse)
-  .delete(deleteCourse);
+// /api/user/:userId
+router.route('/:userId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// /api/user/:userId/reaction
+router.route('/:studentId/reaction').post(addReaction);
+
+// /api/user/:userId/reaction/:reactionId
+router.route('/:userId/reaction/:reactionId').delete(terminateReaction);
 
 module.exports = router;
